f158ad99501c1defab0732784ee2e594b7c4329d
Fernando Cases <nandocasesgarcia@gmail.com>
2024-04-03 01:38:56 +0200

    refactor: Updated Themes component structure

--- web-terminal/src/components/commands/Themes.tsx
+++ web-terminal/src/components/commands/Themes.tsx
@@ -1,44 +1,31 @@
-import { useContext, useEffect } from "react";
-import _ from "lodash";
+import React, { useContext, useEffect } from "react";
 import { themeContext } from "../../App";
 import { Wrapper } from "../styles/Output.styled";
 import { ThemeSpan, ThemesWrapper } from "../styles/Themes.styled";
-import {
-    checkThemeSwitch,
-    getCurrentCmdArry,
-    isArgInvalid,
-} from "../../utils/funcs";
+import { checkThemeSwitch, getCurrentCmdArry, isArgInvalid } from "../../utils/funcs";
 import { termContext } from "../Terminal";
 import theme from "../styles/themes";
 import Usage from "../Usage";
 
-const myThemes = _.keys(theme);
-
 const Themes: React.FC = () => {
     const { arg, history, rerender } = useContext(termContext);
-
     const themeSwitcher = useContext(themeContext);
-
-    /* ===== get current command ===== */
     const currentCommand = getCurrentCmdArry(history);
 
-    /* ===== check current command makes redirect ===== */
     useEffect(() => {
-        if (checkThemeSwitch(rerender, currentCommand, myThemes)) {
+        if (checkThemeSwitch(rerender, currentCommand, Object.keys(theme))) {
             themeSwitcher?.(theme[currentCommand[2]]);
         }
     }, [arg, rerender, currentCommand, themeSwitcher]);
 
-    /* ===== check arg is valid ===== */
-    const checkArg = () =>
-        isArgInvalid(arg, "set", myThemes) ? <Usage cmd="themes" /> : null;
+    if (arg.length > 0 || arg.length > 2) {
+        return isArgInvalid(arg, "set", Object.keys(theme)) ? <Usage cmd="themes" /> : null;
+    }
 
-    return arg.length > 0 || arg.length > 2 ? (
-        checkArg()
-    ) : (
+    return (
         <Wrapper data-testid="themes">
             <ThemesWrapper>
-                {myThemes.map(myTheme => (
+                {Object.keys(theme).map(myTheme => (
                     <ThemeSpan key={myTheme}>{myTheme}</ThemeSpan>
                 ))}
             </ThemesWrapper>
