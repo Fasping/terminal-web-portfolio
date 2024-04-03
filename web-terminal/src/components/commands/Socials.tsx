6a74a10cee373c4676220498ed168d9245c08c27
Fernando Cases <nandocasesgarcia@gmail.com>
2024-04-04 00:00:19 +0200

    Refactor Socials component for readability and organization

--- web-terminal/src/components/commands/Socials.tsx
+++ web-terminal/src/components/commands/Socials.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import React, { useContext, useEffect } from "react";
 import { ProjectsIntro } from "../styles/Projects.styled";
 import { Cmd, CmdDesc, CmdList, HelpWrapper } from "../styles/Help.styled";
 import {
@@ -9,44 +9,6 @@ import {
 } from "../../utils/funcs";
 import { termContext } from "../Terminal";
 import Usage from "../Usage";
-
-const Socials: React.FC = () => {
-    const { arg, history, rerender } = useContext(termContext);
-
-    /* ===== get current command ===== */
-    const currentCommand = getCurrentCmdArry(history);
-
-    /* ===== check current command makes redirect ===== */
-    useEffect(() => {
-        if (checkRedirect(rerender, currentCommand, "socials")) {
-            socials.forEach(({ id, url }) => {
-                id === parseInt(arg[1]) && window.open(url, "_blank");
-            });
-        }
-    }, [arg, rerender, currentCommand]);
-
-    /* ===== check arg is valid ===== */
-    const checkArg = () =>
-        isArgInvalid(arg, "go", ["1", "2", "3", "4"]) ? (
-            <Usage cmd="socials" />
-        ) : null;
-
-    return arg.length > 0 || arg.length > 2 ? (
-        checkArg()
-    ) : (
-        <HelpWrapper data-testid="socials">
-            <ProjectsIntro>Here are my social links</ProjectsIntro>
-            {socials.map(({ id, title, url, tab }) => (
-                <CmdList key={title}>
-                    <Cmd>{`${id}. ${title}`}</Cmd>
-                    {generateTabs(tab)}
-                    <CmdDesc>- {url}</CmdDesc>
-                </CmdList>
-            ))}
-            <Usage cmd="socials" marginY />
-        </HelpWrapper>
-    );
-};
 
 const socials = [
     {
@@ -62,5 +24,47 @@ const socials = [
         tab: 3,
     },
 ];
+
+const Socials: React.FC = () => {
+    const { arg, history, rerender } = useContext(termContext);
+    const currentCommand = getCurrentCmdArry(history);
+
+    useEffect(() => {
+        if (checkRedirect(rerender, currentCommand, "socials")) {
+            const selectedSocial = socials.find(({ id }) => id === parseInt(arg[1]));
+            if (selectedSocial) {
+                window.open(selectedSocial.url, "_blank");
+            }
+        }
+    }, [arg, rerender, currentCommand]);
+
+    const renderSocials = () => {
+        return socials.map(({ id, title, url, tab }) => (
+            <CmdList key={id}>
+                <Cmd>{`${id}. ${title}`}</Cmd>
+                {generateTabs(tab)}
+                <CmdDesc>- {url}</CmdDesc>
+            </CmdList>
+        ));
+    };
+
+    const renderContent = () => {
+        if (arg.length > 0 || arg.length > 2) {
+            return isArgInvalid(arg, "go", ["1", "2", "3", "4"]) ? (
+                <Usage cmd="socials" />
+            ) : null;
+        } else {
+            return (
+                <HelpWrapper data-testid="socials">
+                    <ProjectsIntro>Here are my social links</ProjectsIntro>
+                    {renderSocials()}
+                    <Usage cmd="socials" marginY />
+                </HelpWrapper>
+            );
+        }
+    };
+
+    return renderContent();
+};
 
 export default Socials;
